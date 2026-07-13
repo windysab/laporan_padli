654971620ebc1c1ba29786d6b6544081b2680ddd
azkajihan09 <azkajihan09@gmail.com>
2026-07-13 14:12:47 +0800

    d

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://192.168.1.11/laporan_perkara/';
-$config['base_url'] = 'http://localhost:8080/laporan_padli/';
+$config['base_url'] = 'http://localhost:8080/laporan_pal/';
 
 /*
 |--------------------------------------------------------------------------
